d431fb32a586cbc6805c5b0a0b49bbb989cdbec9
anonimuskamil <anonimuskamil@yandex.ru>
2022-11-22 10:13:19 +0300

    backend

--- myproject/server/node.js
+++ myproject/server/node.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
@@ -8,7 +8,7 @@ const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 
 const app = express();
 app.use(cors());
